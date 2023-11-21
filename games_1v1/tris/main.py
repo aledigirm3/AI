8674e858a9f59a0b00e3fbc6c0853c49df8d6bbe
aledigirm3 <alessandro.digirolamo.1937@gmail.com>
2023-11-21 21:43:36 +0100

    alpha-beta proning method implemented for tris game

--- games_1v1/tris/main.py
+++ games_1v1/tris/main.py
@@ -1,5 +1,9 @@
 from Game import Game
+from GameAlphaBeta import GameAlphaBeta
 
 game = Game()
+gameAlphaBeta = GameAlphaBeta()
 
-game.play()
+
+# game.play() #O(b^h), full tree visited
+gameAlphaBeta.play()  # computationally optimized
